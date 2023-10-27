894a7dadae3df91ec32d6e92ce7528fb5368223e
Leo Li <leo4life@163.com>
2023-10-27 16:12:23 -0400

    chirp v2

--- src/Chirp.sol
+++ src/Chirp.sol
@@ -3,27 +3,75 @@ pragma solidity ^0.8.0;
 
 contract Chirp {
 
-    struct Peep {
+    struct Comment {
         address user;
-        string cid;  // IPFS CID pointing to the peep content
+        string cid;  // IPFS CID pointing to the comment content
         uint256 timestamp;
     }
 
-    Peep[] public peeps;
+    struct Peep {
+        uint256 index;
+        address user;
+        string cid;  // IPFS CID pointing to the peep content
+        uint256 timestamp;
+        uint256 likeCount;  // Counter for number of likes
+        uint256 commentCount;  // Counter for number of comments
+    }
 
-    event NewPeep(address indexed user, string cid, uint256 timestamp);
+    Peep[] public peeps;
+    uint256 public peepIndex = 0;  // Global peep index to track each Peep
+
+    mapping(uint256 => mapping(address => bool)) public hasLiked;
+    mapping(uint256 => Comment[]) public peepComments;
+
+    event NewPeep(uint256 indexed peepIndex, address indexed user, string cid, uint256 timestamp);  
+    event NewLike(address indexed user, uint256 indexed peepIndex);
+    event RemovedLike(address indexed user, uint256 indexed peepIndex);
+    event NewComment(address indexed user, uint256 indexed peepIndex, string cid, uint256 timestamp);  // Modified to use CID
 
     function postPeep(string memory _cid) external {
         peeps.push(Peep({
+            index: peepIndex,
             user: msg.sender,
             cid: _cid,
-            timestamp: block.timestamp
+            timestamp: block.timestamp,
+            likeCount: 0,
+            commentCount: 0
         }));
-        emit NewPeep(msg.sender, _cid, block.timestamp);
+        emit NewPeep(peepIndex, msg.sender, _cid, block.timestamp);
+        peepIndex += 1;
     }
 
-    function getPeep(uint256 index) public view returns (address, string memory, uint256) {
-        Peep memory peep = peeps[index];
-        return (peep.user, peep.cid, peep.timestamp);
+    function likePeep(uint256 _peepIndex) external {
+        require(_peepIndex < peeps.length, "Peep index out of bounds");
+        
+        if (hasLiked[_peepIndex][msg.sender]) {
+            hasLiked[_peepIndex][msg.sender] = false;
+            peeps[_peepIndex].likeCount -= 1;
+            emit RemovedLike(msg.sender, _peepIndex);
+        } else {
+            hasLiked[_peepIndex][msg.sender] = true;
+            peeps[_peepIndex].likeCount += 1;
+            emit NewLike(msg.sender, _peepIndex);
+        }
+    }
+
+    function commentOnPeep(uint256 _peepIndex, string memory commentCid) external {
+        require(_peepIndex < peeps.length, "Peep index out of bounds");
+        
+        Comment memory newComment = Comment({
+            user: msg.sender,
+            cid: commentCid,  // Storing the CID of the comment
+            timestamp: block.timestamp
+        });
+
+        peepComments[_peepIndex].push(newComment);
+        peeps[_peepIndex].commentCount += 1;
+
+        emit NewComment(msg.sender, _peepIndex, commentCid, block.timestamp);
+    }
+
+    function getComments(uint256 _peepIndex) external view returns (Comment[] memory) {
+        return peepComments[_peepIndex];
     }
 }
--- test/Chirp.t.sol
+++ test/Chirp.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.0;
 
 import {Test} from "forge-std/Test.sol";
 import {Chirp} from "../src/Chirp.sol";
@@ -11,12 +11,37 @@ contract ChirpTest is Test {
         chirp = new Chirp();
     }
 
-    function test_PostPeep() public {
-        string memory testCID = "QmT78zSuBmuS4z925WZfrqQ1qHaJ56DQaTfyMUF7F8ff5o"; // Example CID
-        chirp.postPeep(testCID);
+    function testPostPeep() public {
+        string memory cid = "QmT78zSuBmuS4z925WZfrqQ1qHaJ56DQaTfyMUF7F8ff5o";
+        
+        chirp.postPeep(cid);
 
-        (address user, string memory cid, uint256 timestamp) = chirp.getPeep(0);
-        assertEq(user, address(this));
-        assertEq(cid, testCID);
+        (, address user, string memory peepCid, , uint256 likeCount, uint256 commentCount) = chirp.peeps(0);
+        assertEq(user, address(this), "Peep poster address mismatch");
+        assertEq(peepCid, cid, "Peep content ID mismatch");
+        assertEq(likeCount, 0, "Initial like count should be 0");
+        assertEq(commentCount, 0, "Initial comment count should be 0");
     }
+
+    function testLikeAndUnlikePeep() public {
+        chirp.postPeep("ExampleCID");
+
+        chirp.likePeep(0);
+        (, , , , uint256 likesAfterLike, ) = chirp.peeps(0);
+        assertEq(likesAfterLike, 1, "Like count should be 1 after a like");
+
+        chirp.likePeep(0);  // Unlike
+        (, , , , uint256 likesAfterUnlike, ) = chirp.peeps(0);
+        assertEq(likesAfterUnlike, 0, "Like count should be 0 after unlike");
+    }
+
+    function testCommentOnPeep() public {
+        chirp.postPeep("AnotherExampleCID");
+        string memory comment = "This is a test comment!";
+
+        chirp.commentOnPeep(0, comment);
+        (, , , , , uint256 commentsAfterComment) = chirp.peeps(0);
+        assertEq(commentsAfterComment, 1, "Comment count should be 1 after a comment");
+    }
+
 }
